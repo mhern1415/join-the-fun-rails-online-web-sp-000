8a077e115da782abe97145b34ede85dd78fe043a
Mark Hernandez <m.hern1415@gmail.com>
2019-12-12 00:40:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
+ActiveRecord::Schema.define(version: 20191212003209) do
+
+  create_table "passenger_taxis", force: :cascade do |t|
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
