afb2b22b22ed2422848ef73c937e273f49edd1c1
Mark Hernandez <m.hern1415@gmail.com>
2019-12-12 00:34:58 +0000

    Automatically backed up by Learn

--- db/migrate/20191212003209_create_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTable < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :, : do |t|
-      # t.index [:_id, :_id]
-      # t.index [:_id, :_id]
-    end
-  end
-end
